3048b55fafe5e8ae9a56a91fa072209197f76077
Hung <hoangvanhung6876@gmail.com>
2023-06-09 16:21:33 +0700

    chore: add dashboard to orgin allwed

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:42467',
+      'https://dashboard.devhungops.website',
     ],
     allowedHeaders: 'Content-Type',
     credentials: true,
